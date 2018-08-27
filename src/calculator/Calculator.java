df76e688baf84f571ef95dc5e6f2e5432e42a6e6
Aleksejs <alex@inbox.lv>
2018-08-27 20:46:43 +0300

    Deleted comment

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -1,10 +1,5 @@
 package calculator;
 
-/**
- * @author Dimitrijs Fedotovs <a href="http://www.bug.guru">www.bug.guru</a>
- * @version 1.0
- * @since 1.0
- */
 class Calculator {
 
     String calculate(String[] expression) {
