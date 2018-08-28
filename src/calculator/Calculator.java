2bc8b0acc8bfd8057875ff4640d4dce125386fa6
alektomi <aleksejs.tomisevs@inbox.lv>
2018-08-28 18:50:05 +0300

    Updated code with FOR operator. Not correct order of operation

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -3,50 +3,26 @@ package calculator;
 class Calculator {
 
     String calculate(String[] expression) {
-        System.out.println(expression[0]);
-        System.out.println(expression[1]);
-        System.out.println(expression[2]);
-
-        double a = Double.parseDouble(expression[0]);
-        double b = Double.parseDouble(expression[2]);
-        double result;
-        switch (expression[1]) {
-            case "+":
-                result = a + b;
-                break;
-            case "-":
-                result = a - b;
-                break;
-            case "*":
-                result = a * b;
-                break;
-            case "/":
-                result = a / b;
-                break;
-            default:
-                return "error";
-        }
-        if (expression.length <= 3 ) {
-            return String.valueOf(result);
-        }
-        System.out.println(expression[3]);
-        System.out.println(expression[4]);
-        double c = Double.parseDouble(expression[4]);
-        switch (expression[3]) {
-            case "+":
-                result = result + c;
-                break;
-            case "-":
-                result = result - c;
-                break;
-            case "*":
-                result = result * c;
-                break;
-            case "/":
-                result = result / c;
-                break;
-            default:
-                return "error";
+        double result = Double.parseDouble(expression[0]);
+        for (int i = 1; i < expression.length; i = i + 2) {
+            String op = expression[i];
+            double b = Double.parseDouble(expression[i + 1]);
+            switch (op) {
+                case "+":
+                    result += b; // result=result + b
+                    break;
+                case "-":
+                    result -= b; // result=result - b
+                    break;
+                case "*":
+                    result *= b; // result=result * b
+                    break;
+                case "/":
+                    result /= b; // result=result / b
+                    break;
+                default:
+                    return "ERROR";
+            }
         }
         return String.valueOf(result);
     }
