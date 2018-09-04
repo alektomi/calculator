b0559478d091dd5aee5e7890edcf7489fd624242
alektomi <aleksejs.tomisevs@inbox.lv>
2018-09-04 18:44:34 +0300

    Izveidojām LIST un tagad notiek dalīšana un reizināšana

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -1,12 +1,40 @@
 package calculator;
 
+import java.util.ArrayList;
+import java.util.List;
+
 class Calculator {
 
     String calculate(String[] expression) {
-        double result = Double.parseDouble(expression[0]);
-        for (int i = 1; i < expression.length; i = i + 2) {
+        List<String> tmp = new ArrayList<>(); // izveidojām jaunu saraksu. darbojās kā masīvs, bet ir ērtāks. Masīvs vienmēr ir statisks, List var papildināt, jo tas ir dinamiskss
+
+        double a = Double.parseDouble(expression[0]);
+        for (int i = 1; i < expression.length; i += 2) {
             String op = expression[i];
             double b = Double.parseDouble(expression[i + 1]);
+            switch (op) {
+                case "+":
+                case "-":
+                    tmp.add(String.valueOf(a));
+                    tmp.add(op);
+                    a = b;
+                    break;
+                case "*":
+                    a *= b;
+                    break;
+                case "/":
+                    a /= b;
+                    break;
+            }
+        }
+        tmp.add(String.valueOf(a));
+
+        System.out.println(tmp);
+
+        double result = Double.parseDouble(tmp.get(0));
+        for (int i = 1; i < tmp.size(); i = i + 2) {
+            String op = tmp.get(i);
+            double b = Double.parseDouble(tmp.get(i + 1));
             switch (op) {
                 case "+":
                     result += b; // result=result + b
@@ -14,12 +42,12 @@ class Calculator {
                 case "-":
                     result -= b; // result=result - b
                     break;
-                case "*":
-                    result *= b; // result=result * b
-                    break;
-                case "/":
-                    result /= b; // result=result / b
-                    break;
+//                case "*":
+//                    result *= b; // result=result * b
+//                    break;
+//                case "/":
+//                    result /= b; // result=result / b
+//                    break;
                 default:
                     return "ERROR";
             }
